d0539980a7dcb5c0d6c7516230f5b5c013554430
srikanthreddybattula <srikantreddy66@gmail.com>
2018-08-07 23:10:16 +0530

    Commit for develop branch

--- GitTutorial/src/main/java/com/git/tutorial/Test.java
+++ GitTutorial/src/main/java/com/git/tutorial/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This  is Git tutorial");
 		System.out.println("Added for checking commit");
-		
+		System.out.println("Added for Develop branch");
 	}
 }
