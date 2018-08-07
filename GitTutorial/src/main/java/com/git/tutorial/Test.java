5c925e5bd9a229c41fb9b365acf20f5cd5d3e37f
srikanthreddybattula <srikantreddy66@gmail.com>
2018-08-07 23:06:40 +0530

    test commit

--- GitTutorial/src/main/java/com/git/tutorial/Test.java
+++ GitTutorial/src/main/java/com/git/tutorial/Test.java
@@ -3,5 +3,7 @@ package com.git.tutorial;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("This  is Git tutorial");
+		System.out.println("Added for checking commit");
+		
 	}
 }
